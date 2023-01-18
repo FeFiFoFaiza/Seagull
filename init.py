3a0bca27d691f7b4325cd5a237aa680fcf77ddb4
Faiza Huda <fhuda30@stuy.edu>
2023-01-17 21:24:27 -0500

    work in progress

--- init.py
+++ init.py
@@ -7,11 +7,11 @@ exImg = mpimg.imread('seagull.JPG')
 
 # Prints out the matrix representation of the picture 
 # where every inner list represents a pixel with its RGB code
-print(exImg)
+#print(exImg)
 
 # Image is graphed using pyplot
-exPlot = plt.imshow(exImg)
-plt.show()
+#exPlot = plt.imshow(exImg)
+#plt.show()
 
 # Storing the dimensions (m x n) of the image
 m = exImg.shape[1]
@@ -21,12 +21,53 @@ print("The dimensions of this image is: " + str(m) + " by " + str(n))
 # Cropping the image 
 ImJPG_center = exImg[100:m-100, 100:n-100]
 
-# Creating a function that will transpose matrices
+# # Creating a function that will transpose matrices
+# def transpose(matrix):
+#     cols = len(matrix)
+#     rows = len(matrix[0])
+
+#     newMatrix = [[0 for i in range(cols)] for j in range(rows)]
+
+#     for i in range (rows):
+#         for j in range (cols):
+#             newMatrix[j][i] = 0
+#             for i in range(len(x)):
+#                 #Iterate through columns
+#                 for j in range(len(x[0])):
+#                     result[j][i] = x[i][j]
+#                     for r in Result
+#                     print(r)
+#     #return newMatrix
+#     return rows
+    
+# Rotates and flips
 def transpose(matrix):
     rows = len(matrix)
     cols = len(matrix[0])
-    
-transpose(exImg)
+    newMatrix = []
+    for i in range(cols):
+        newRow = []
+        for j in range(rows):
+            newRow.append(matrix[j][i])
+        newMatrix.append(newRow)
+    return newMatrix
 
-plt.imshow(ImJPG_center)
+# Reflects over the y-axis
+def vertFlip(matrix):
+    return [ row[::-1] for row in matrix ]
+
+# Rotates it 90 deg clockwise
+def rotate90CW(matrix):
+    return vertFlip(transpose(matrix))
+
+# Rotates it 90 deg counter clockwise
+def rotate90CCW(matrix):
+    return transpose(vertFlip(matrix))
+
+plt.imshow(rotate90CCW(exImg))
 plt.show()
+
+#print(transpose([[2,3,4],[5,2,3]]))
+
+#plt.imshow(ImJPG_center)
+#plt.show()
